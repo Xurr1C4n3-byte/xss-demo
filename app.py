74e1ebc3b8baabf1928c942b13b3a9cc7d7f1a05
Xurr1C4n3-byte <xurr1C4n3@gmail.com>
2025-07-27 03:27:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import Flask, send_file
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
+    return "Go to /index.html"
+
+@app.route('/index.html')
+def serve_index():
     return send_file('index.html')
